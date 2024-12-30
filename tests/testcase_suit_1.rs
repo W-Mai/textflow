ff36b1e66eb548d7dc391a1e7775b737399cd866
Benign X <1341398182@qq.com>
2024-12-30 21:20:39 +0800

    💖(tests): add tests for multilingual and special characters
    New tests have been added to enhance coverage for various multilingual strings and special characters within the `assert_flow!` function. This ensures robustness in handling diverse inputs, including Chinese characters and punctuation, improving overall functionality and reliability.
    
    Signed-off-by: Benign X <1341398182@qq.com>

--- tests/testcase_suit_1.rs
+++ tests/testcase_suit_1.rs
@@ -31,7 +31,79 @@ mod testcase_suit_1 {
 
     #[test]
     fn test_3() {
+        assert_flow!("你好中国", ["你", "好", "中", "国"], 2);
+    }
+
+    #[test]
+    fn test_4() {
         assert_flow!("Hello, world!", ["Hello, ", "world!"], 10);
+    }
+
+    #[test]
+    fn test_5() {
         assert_flow!("Hello, world!", ["Hel", "lo,", "wor", "ld!"], 3);
+    }
+
+    #[test]
+    fn test_6() {
+        assert_flow!("This is a Text》〉>?!", ["This is a ", "Text》〉>?!"], 20);
+    }
+
+    #[test]
+    fn test_7() {
+        assert_flow!("<〈《Teext a>>>", ["<〈《Teext ", "a>>>"], 12);
+    }
+
+    #[test]
+    fn test_8() {
+        assert_flow!("<〈《Tee<ext><>>", ["<〈《Tee", "<ext><>>"], 12);
+    }
+
+    #[test]
+    fn test_9() {
+        assert_flow!("<〈《Tee<eext><>>", ["<〈《Tee", "<eext><>>"], 12);
+    }
+
+    #[test]
+    fn test_10() {
+        assert_flow!("<〈<<《你》>", ["<〈<<《你", "》>"], 10);
+    }
+
+    #[test]
+    fn test_11() {
+        assert_flow!("<〈<<《Loooooo｜ong>>", ["<〈<<《Loooooo", "｜ong>>"], 14);
+    }
+
+    #[test]
+    fn test_12() {
+        assert_flow!(
+            "this is aaaa \"text word\" test",
+            ["this is aaaa ", "\"text word\" ", "test"],
+            15
+        );
+    }
+
+    #[test]
+    fn test_13() {
+        assert_flow!(
+            "this is a \"text word\" test",
+            ["this is a \"text", "word\" test"],
+            15
+        );
+    }
+
+    #[test]
+    fn test_14() {
+        assert_flow!("this is a <text> test", ["this is a ", "<text> test"], 15);
+    }
+
+    #[test]
+    fn test_15() {
+        assert_flow!("this is a text-test", ["this is a text-", "test"], 15);
+    }
+
+    #[test]
+    fn test_16() {
+        assert_flow!("实时操作系统 Nuttx》。", ["实时操作系统 ", "Nuttx》。"], 20);
     }
 }
